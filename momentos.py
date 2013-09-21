cc07af101ce5ef6c7f7e5f7ef0299d3450924f50
Kareem Shehata <kshehata@Kareem-MacBookPro-3.local>
2013-09-21 15:20:23 -0400

    First version of storing and retrieving momentos. Use the debug interface for examples.

---
+++ momentos.py
@@ -0,0 +1,100 @@
+import os
+import webapp2
+import cgi
+import urllib
+import jinja2
+import json
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'])
+
+class Momento(ndb.Model):
+    """Models an individual Guestbook entry with author, content, and date."""
+    author = ndb.UserProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
+    text = ndb.StringProperty(indexed=False)
+    location = ndb.GeoPtProperty()
+    image = ndb.BlobProperty(default=None)
+    thumbnail = ndb.BlobProperty(default=None)
+
+    def serialize(self, image=False):
+        d = {
+            'key' : self.key.urlsafe(),
+            'date' : self.date.isoformat(),
+            'text' : self.text,
+            'location' : self.location,
+            'thumbnail' : self.thumbnail,
+        }
+        if self.author:
+            d['author'] = self.author.nickname()
+        else:
+            d['author'] = None
+        if image:
+            d['image'] = self.image
+        return d
+
+
+class DebugPage(webapp2.RequestHandler):
+
+    def get(self):
+        if users.get_current_user():
+            url = users.create_logout_url(self.request.uri)
+            url_linktext = 'Logout'
+        else:
+            url = users.create_login_url(self.request.uri)
+            url_linktext = 'Login'
+
+        template_values = {
+            'url': url,
+            'url_linktext': url_linktext,
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('debug.html')
+        self.response.write(template.render(template_values))
+
+
+class GetMomentos(webapp2.RequestHandler):
+    def get(self):
+        momento_query = Momento.query()
+        momentos = momento_query.fetch(20)
+        self.response.headers['Content-Type'] = 'application/json'
+        momento_list = [ m.serialize() for m in momentos ]
+        obj = {
+            'momentos': momento_list, 
+          }
+        self.response.out.write(json.dumps(obj))
+
+class GetMomento(webapp2.RequestHandler):
+    def get(self):
+        key = ndb.Key(urlsafe=self.request.get('momento_id'))
+        momento = key.get()
+        obj = {
+            'id' : self.request.get('momento_id'),
+            'momento': momento.serialize(image=True), 
+          }
+        self.response.out.write(json.dumps(obj))
+
+class PostMomento(webapp2.RequestHandler):
+    def post(self):
+        momento = Momento()
+
+        if users.get_current_user():
+            momento.author = users.get_current_user()
+
+        momento.text = self.request.get('text')
+        momento.put()
+
+        self.redirect('/debug')
+
+application = webapp2.WSGIApplication([
+    ('/debug', DebugPage),
+    ('/add_momento', PostMomento),
+    ('/get_momentos', GetMomentos),
+    ('/momento', GetMomento),
+], debug=True)
+
